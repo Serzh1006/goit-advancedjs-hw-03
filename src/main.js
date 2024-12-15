e4d1f593362bbdda8587cb3b83a54fa8b36b499e
Sergey Kravchenko <skravchenko223@gmail.com>
2024-12-15 22:17:35 +0100

    add styles on form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import iziToast from 'izitoast';
+import SimpleLightbox from 'simplelightbox';
 import { fetchByValue } from './js/pixabay-api.js';
 import { renderBySearch } from './js/render-functions.js';
+import 'izitoast/dist/css/iziToast.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.form');
@@ -8,15 +12,22 @@ form.addEventListener('submit', e => {
   e.preventDefault();
   const textByUser = e.target.elements[0].value;
   if (textByUser.trim() === '') {
-    console.log('Please enter text!');
+    iziToast.info({
+      title: 'Oops...',
+      message: 'Please enter text!',
+      position: 'topRight',
+    });
     return;
   }
   fetchByValue(textByUser.trim())
     .then(data => {
       if (data.hits.length === 0) {
-        console.log(
-          'Sorry, there are no images matching your search query. Please try again!'
-        );
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'bottomCenter',
+        });
         gallery.innerHTML = '';
         form.reset();
         return;
@@ -26,4 +37,10 @@ form.addEventListener('submit', e => {
       form.reset();
     })
     .catch(err => console.log(err));
+});
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
 });
